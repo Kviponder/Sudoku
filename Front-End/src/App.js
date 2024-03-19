34292c5e99a51f90129799126b7e0922319940f4
Shane O'Brien <shane.g.obrien@gmail.com>
2024-03-19 16:26:07 -0400

    added pause, solve, and new buttons and styles

--- Front-End/src/App.js
+++ Front-End/src/App.js
@@ -31,6 +31,10 @@ function App() {
     setBoard(grid);
   }
 
+  function checkBoard() {
+    console.log("check");
+  }
+
   useEffect(() => {
     let interval;
     if (running) {
@@ -55,33 +59,42 @@ function App() {
             <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}</span>
           </div>
         </div>
-        <table className="board">
-          <tbody>
-            {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((row, rIndex) => {
-              return (
-                <tr key={rIndex}>
-                  {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((col, cIndex) => {
-                    return (
-                      <td key={rIndex + cIndex}>
-                        <input
-                          value={
-                            sudoBoard[row][col] === -1
-                              ? ""
-                              : sudoBoard[row][col]
-                          }
-                          onChange={(e) => onInputChange(e, row, col)}
-                          className="board__cell"
-                          disabled={sudoBoard[row][col] !== -1}
-                        />
-                      </td>
-                    );
-                  })}
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
       </header>
+      <table className="board">
+        <tbody>
+          {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((row, rIndex) => {
+            return (
+              <tr key={rIndex}>
+                {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((col, cIndex) => {
+                  return (
+                    <td key={rIndex + cIndex}>
+                      <input
+                        value={
+                          sudoBoard[row][col] === -1 ? "" : sudoBoard[row][col]
+                        }
+                        onChange={(e) => onInputChange(e, row, col)}
+                        className="board__cell"
+                        disabled={sudoBoard[row][col] !== -1}
+                      />
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <div className="button__container">
+        <button className="button" onClick={checkBoard}>
+          Pause
+        </button>
+        <button className="button" onClick={checkBoard}>
+          Solve
+        </button>
+        <button className="button" onClick={checkBoard}>
+          New
+        </button>
+      </div>
     </div>
   );
 }
